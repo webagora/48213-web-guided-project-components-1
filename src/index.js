361bbc93f367d7db0c80da4c54929eecbdc90a5b
Casey Harding <casey.harding@generalassemb.ly>
2021-09-01 12:44:12 -0400

    Through step 7.

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ imageData.forEach(imgObj => {
 //  Destructure `open` and `close` from the constants
 import panelData from './data/panelData.js';
 import constants from './data/constants.js';
-import linkData from './data/linkData';
+import linkData from './data/linkData.js';
 
 // TASK 2- Verify our imports using log statements
 console.log(panelData); // log the panelData
@@ -39,21 +39,21 @@ console.log(linkData); // log the close arrow
 // TASK 3- Comment out the div.panel from index.html and grab its parent element.
 //  We will generate the panel with code, and we'll need the parent
 //  so we can append the code-generated panel to the DOM.
-const accordion = null
+const accordion = document.querySelector('.accordion');
 
 
 // TASK 4- Create a function 'makePanel' that creates a panel exactly as you see it in the HTML.
-function makePanel(/* what data does the panel need? */) {
+function makePanel({ title, content }) {
 
 
   // TASK 5- Instantiate all the elements needed for a panel
-  const panel = null
-  const panelBar = null
-  const panelContent = null
-  const panelTitle = null
-  const panelButtons = null
-  const openButton = null
-  const closeButton = null
+  const panel = document.createElement('div');
+  const panelBar = document.createElement('div');
+  const panelContent = document.createElement('div');
+  const panelTitle = document.createElement('h3');
+  const panelButtons = document.createElement('div');
+  const openButton = document.createElement('button');
+  const closeButton = document.createElement('button');
 
 
   // TASK 6- Setup the structure of our elements
@@ -69,12 +69,37 @@ function makePanel(/* what data does the panel need? */) {
       <div></div>           // panelContent
     </div>
   */
-
+  panel.appendChild(panelBar);
+  panel.appendChild(panelContent);
+  panelBar.appendChild(panelTitle);
+  panelBar.appendChild(panelButtons);
+  panelButtons.appendChild(openButton);
+  panelButtons.appendChild(closeButton);
 
   // TASK 7- Add proper class names to our elements (See index.html for reference)
   // paying attention to the elements that need to start out hidden
+        // <!-- Remove this -->
+        // <div class="panel">
+        //   <div class="panel-bar">
+        //     <h3>Title of Panel</h3>
+        //     <div class="panel-buttons">
+        //       <button class="panel-btn-open">&#9660</button>
+        //       <button class="panel-btn-close hide-btn">Close</button>
+        //     </div>
+        //   </div>
+        //   <div class="panel-content toggle-on">
+        //     Content of panel
+        //   </div>
+        // </div>
+        // <!-- Remove this -->
+  panel.classList.add('panel');
+  panelBar.classList.add('panel-bar');
+  panelButtons.classList.add('panel-buttons');
+  openButton.classList.add('panel-btn-open');
+  closeButton.classList.add('panel-btn-close', 'hide-btn');
+  panelContent.classList.add('panel-content');
 
-
+  
   // TASK 8- Set text content using arguments as raw material
   //  and also using the open and close arrows imported at the top of the file
 
