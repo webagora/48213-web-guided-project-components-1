9e2d8728f5b4f48a48732d3a6d0ba854c9348be6
Casey Harding <casey.harding@generalassemb.ly>
2021-09-01 12:55:34 -0400

    Through step 9.

--- src/index.js
+++ src/index.js
@@ -35,6 +35,9 @@ console.log(panelData); // log the panelData
 console.log(constants); // log the open arrow
 console.log(linkData); // log the close arrow
 
+const { open, close } = constants; // {open: 'asdf', close: 'blah'}
+// const open = constants.open
+// const close = constants.close
 
 // TASK 3- Comment out the div.panel from index.html and grab its parent element.
 //  We will generate the panel with code, and we'll need the parent
@@ -44,8 +47,6 @@ const accordion = document.querySelector('.accordion');
 
 // TASK 4- Create a function 'makePanel' that creates a panel exactly as you see it in the HTML.
 function makePanel({ title, content }) {
-
-
   // TASK 5- Instantiate all the elements needed for a panel
   const panel = document.createElement('div');
   const panelBar = document.createElement('div');
@@ -78,20 +79,6 @@ function makePanel({ title, content }) {
 
   // TASK 7- Add proper class names to our elements (See index.html for reference)
   // paying attention to the elements that need to start out hidden
-        // <!-- Remove this -->
-        // <div class="panel">
-        //   <div class="panel-bar">
-        //     <h3>Title of Panel</h3>
-        //     <div class="panel-buttons">
-        //       <button class="panel-btn-open">&#9660</button>
-        //       <button class="panel-btn-close hide-btn">Close</button>
-        //     </div>
-        //   </div>
-        //   <div class="panel-content toggle-on">
-        //     Content of panel
-        //   </div>
-        // </div>
-        // <!-- Remove this -->
   panel.classList.add('panel');
   panelBar.classList.add('panel-bar');
   panelButtons.classList.add('panel-buttons');
@@ -102,16 +89,23 @@ function makePanel({ title, content }) {
   
   // TASK 8- Set text content using arguments as raw material
   //  and also using the open and close arrows imported at the top of the file
-
+  panelTitle.textContent = title;
+  panelContent.textContent = content;
+  openButton.textContent = open;
+  closeButton.textContent = close;
 
   // TASK 9- When the 'open' or 'close' buttons are clicked, the content is toggled on/off:
   //  - the open button needs to go away (the 'hide-btn' class name controls this)
   //  - the close button needs to show (the 'hide-btn' class name controls this)
   //  - the contents need to show (the 'toggle-on' class name controls this)
-
+  panelButtons.addEventListener('click', () => {
+    openButton.classList.toggle('hide-btn');
+    closeButton.classList.toggle('hide-btn');
+    panelContent.classList.toggle('toggle-on');
+  })
 
   // don't forget to return the panel!
-  return null
+  return panel
 }
 
 
